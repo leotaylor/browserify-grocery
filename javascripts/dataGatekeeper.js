c1a4b5141c2d94842b97ba739f0c70adea4616e5
Leo Taylor <leotaylor723@gmail.com>
2018-04-14 12:39:47 -0500

    cleanup

--- javascripts/dataGatekeeper.js
+++ javascripts/dataGatekeeper.js
@@ -1,9 +1,11 @@
 const loadDepts = require('./departments');
 const departmentDom = require('./departmentDom');
+const data = require('./data');
 
 const whenDeptLoads = function () {
-  const data = JSON.parse(this.responseText).departments;
-  departmentDom(data);
+  const departmentsData = JSON.parse(this.responseText).departments;
+  data.setDepartments(departmentsData);
+  departmentDom(departmentsData);
 };
 
 const errorFunction = function () {
